f8e0437ac2fd18ad2ad567fd7a8160bbea7c9350
Caio Yoshida Dell G3 <caio.yoshida@yahoo.com.br>
2021-01-16 16:51:32 -0300

    refresh dist

--- dist/server.js
+++ dist/server.js
@@ -14,6 +14,6 @@ var app = express_1.default();
 app.use(cors_1.default());
 app.use(express_1.default.json());
 app.use(routes_1.default);
-app.listen(3333, function () {
+app.listen(process.env.PORT || 3333, function () {
     console.log('server started on port 3333');
 });
